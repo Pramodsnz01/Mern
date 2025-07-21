6a02b898ecb18eb7f2004c8d7327891d68ce9d7c
Pramodsnz01 <pramodsnz01@gmail.com>
2025-07-21 11:37:54 +0545

    Promise

--- JS/promise/practice.js
+++ JS/promise/practice.js
@@ -9,3 +9,43 @@ promise1.then(function(){
     console.log("Consumed");
     
 })
+
+const prom2 = new Promise(function(res, rej){
+    setTimeout(function(){
+        res({username: "ram"})
+    }, 2000);
+})
+prom2.then(function(user){
+    console.log(user);
+})
+
+const prom3 = new Promise(function(res, rej){
+    setTimeout(function(){
+        let error = false
+        if (!error) {
+            res({username:"Hari", address:"Ktm"})
+        }else{
+            rej('Error: Something went wrong')
+        }
+    }, 3000);
+})
+// prom3.then((user)=>{
+//     console.log(user);
+//     return user.address
+// }).then((username)=>{
+//     console.log(username);
+    
+// }).catch(function(error){
+//     console.log(error);
+    
+// })
+
+async function consProm3(){
+    try{
+        const response = await prom3
+        console.log(response)
+    }catch(error){
+        console.log(error);
+    }
+}
+consProm3()
